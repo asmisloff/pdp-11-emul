530f87a9419656b5ee7137ebba0afa04b8d2c38b
Smyslov.Aleksey <a.smisloff@gmail.com>
2025-05-05 11:11:54 +0300

    Скрипт запуска адаптирован для MSYS2

--- test/run/run.py
+++ test/run/run.py
@@ -5,6 +5,7 @@
 import sys
 import os
 import pathlib
+import platform
 
 TEST_CPP = sys.argv[1]
 EXE = pathlib.PurePath(TEST_CPP).name[:-4]
@@ -13,6 +14,10 @@ if len(sys.argv) > 1:
     OBJ_FILES = ' '.join(sys.argv[2:])
 CC = 'g++ -std=c++17 -g -Wall -Wextra'
 CMD = f'{CC} {TEST_CPP} {OBJ_FILES} -o ../{EXE}'
+RUN_CMD = f'./{EXE}' if platform.system() == 'Linux' else f'{EXE}.exe'
+RM_CMD = f'rm ../{EXE}' if platform.system() == 'Linux' else f'rm ../{EXE}.exe'
+print(CMD)
 os.system(CMD)
-os.system(f'cd .. && ./{EXE}')
-os.system(f'rm ../{EXE}')
+print(f'cd .. && {RUN_CMD}')
+os.system(f'cd .. && {RUN_CMD}')
+os.system(RM_CMD)
